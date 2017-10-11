2685360f78c3a58e7b8f57dcc2dba7a5b66ffb73
CBrewski <iamsparticus33@gmail.com>
2017-10-11 10:56:16 -0600

    Added a setup file for fedora 26

---
+++ fedoraSetup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Google Chrome
+cat << EOF > /etc/yum.repos.d/google-chrome.repo
+[google-chrome]
+name=google-chrome - \$basearch
+baseurl=http://dl.google.com/linux/chrome/rpm/stable/\$basearch
+enabled=1
+gpgcheck=1
+gpgkey=https://dl-ssl.google.com/linux/linux_signing_key.pub
+EOF
+
+dnf install -y google-chrome-stable
+
+# node
+curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
+dnf -y install nodejs
+
+# VS Code
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+dnf -y check-update
+dnf -y install code
+
