a8165fa458de64b5e8c96ed1a8e41db2ae50eb02
CBrewski <iamsparticus33@gmail.com>
2017-11-08 09:03:58 -0700

    Added node

--- linuxSetup.sh
+++ linuxSetup.sh
@@ -44,3 +44,8 @@ sudo apt-get install -y openssh-server
 
 # vim
 sudo apt-get install -y vim
+
+# node 8
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
