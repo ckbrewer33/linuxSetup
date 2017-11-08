ea5e996cce37f16a001a647c7992dd67830e1fbf
CBrewski <iamsparticus33@gmail.com>
2017-11-08 08:14:18 -0700

    Updated ST3 install, added -y to all isntalls

--- linuxSetup.sh
+++ linuxSetup.sh
@@ -5,7 +5,6 @@
 # Setup git stuff
 sudo apt-get install git
 git config --global user.name "Chris Brewer"
-git config --global user.email ckbrewer33@gmail.com
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 sudo apt-get install meld
 git config --global diff.tool meld
@@ -15,33 +14,33 @@ git config --global merge.tool meld
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update 
-sudo apt-get install google-chrome-stable
-
+sudo apt-get install -y google-chrome-stable
 
 # Sublime Text 3
-sudo add-apt-repository ppa:webupd8team/sublime-text-3
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install -y apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
-sudo apt-get install sublime-text-installer
-
+sudo apt-get install -y sublime-text
 
 # VLC
-sudo apt-get install vlc
-sudo apt-get install mozilla-plugin-vlc
+sudo apt-get install -y vlc
+sudo apt-get install -y mozilla-plugin-vlc
 
 # Java
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install oracle-java8-installer
+sudo apt-get install -y oracle-java8-installer
 
 # Spotify
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update
-sudo apt-get install spotify-client
+sudo apt-get install -y spotify-client
 
 # ssh
 sudo apt-get update
-sudo apt-get install openssh-server
+sudo apt-get install -y openssh-server
 
 # vim
-sudo apt-get install vim
+sudo apt-get install -y vim
