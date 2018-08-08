f90bbddbdc072e8606f1bd79a8c76c941cfb7459
Chris Brewer <chris.brewer@genesys.com>
2018-08-08 15:04:37 -0600

    Update linuxSetup.sh

--- linuxSetup.sh
+++ linuxSetup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Simple setup.sh for configuring my Linux dev environment
 
-
 # Setup git stuff
 sudo apt-get install -y git
 git config --global user.name "Chris Brewer"
@@ -10,11 +9,6 @@ sudo apt-get install meld
 git config --global diff.tool meld
 git config --global merge.tool meld
 
-# Google Chrome
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo apt-get update 
-sudo apt-get install -y google-chrome-stable
 
 # Sublime Text 3
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
@@ -26,11 +20,6 @@ sudo apt-get install -y sublime-text
 # VLC
 sudo apt-get install -y vlc
 sudo apt-get install -y mozilla-plugin-vlc
-
-# Java
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install -y oracle-java8-installer
 
 # Spotify
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
@@ -48,4 +37,3 @@ sudo apt-get install -y vim
 # node 8
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
-
