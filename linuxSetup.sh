3b855bfc904174e6a361c358848f3a13da1fa094
Chris Brewer <chris.brewer@genesys.com>
2020-02-11 06:35:16 -0700

    Added docker, nvm, removed old code

--- linuxSetup.sh
+++ linuxSetup.sh
@@ -4,10 +4,12 @@
 # Setup git stuff
 sudo apt-get install -y git
 git config --global user.name "Chris Brewer"
-git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-sudo apt-get install meld
-git config --global diff.tool meld
-git config --global merge.tool meld
+git config --global user.email "ckbrewer33@gmail.com"
+
+# NVM & node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+source ~/.bashrc
+nvm install 10
 
 
 # Sublime Text 3
@@ -21,11 +23,11 @@ sudo apt-get install -y sublime-text
 sudo apt-get install -y vlc
 sudo apt-get install -y mozilla-plugin-vlc
 
-# Spotify
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update
-sudo apt-get install -y spotify-client
+# # Spotify
+# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+# echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+# sudo apt-get update
+# sudo apt-get install -y spotify-client
 
 # ssh
 sudo apt-get update
@@ -34,6 +36,15 @@ sudo apt-get install -y openssh-server
 # vim
 sudo apt-get install -y vim
 
-# node 8
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get install -y nodejs
+# Docker
+sudo apt-get update
+sudo apt-get remove docker docker-engine docker.io
+sudo apt install docker.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+# aws-cli
+sudo apt--get install awscli
+aws --version
